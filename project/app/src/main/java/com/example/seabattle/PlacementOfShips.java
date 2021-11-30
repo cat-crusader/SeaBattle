45c3ef68b97ee01c281abf00ff44e93bb3c5b3e2
Cat Crusader <cat.crusader31@gmail.com>
2021-12-01 01:42:22 +0200

    start implement drag&drop system for ships

--- project/app/src/main/java/com/example/seabattle/PlacementOfShips.java
+++ project/app/src/main/java/com/example/seabattle/PlacementOfShips.java
@@ -3,14 +3,146 @@ package com.example.seabattle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class PlacementOfShips extends AppCompatActivity {
+public class PlacementOfShips extends AppCompatActivity implements
+        View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        View.OnDragListener{
+
+    private  static final String TAG = "PlacementOfShips";
+
+//      widgets
+    private View layoutView;
+
+
+
+//      vars
+    private GestureDetector mGestureDetector;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placement_of_ships);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        layoutView = findViewById(R.id.layout_ship_placement);
+
+        layoutView.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this,this);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        int action = event.getAction();
+//        switch (action){
+//            case (MotionEvent.ACTION_MOVE):
+//                Log.d(TAG,"onTouch:"+event.getX() +" , "+event.getY());
+//                break;
+//            default:
+//                break;
+//        }
+
+        return true;
+    }
+        /*
+        GestureDetector
+         */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.d(TAG,"Action was onDown");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.d(TAG,"Action was onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.d(TAG,"Action was onSingleTap");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.d(TAG,"Action was onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.d(TAG,"Action was onLongPress");
+        View.DragShadowBuilder builder = new View.DragShadowBuilder(layoutView);
+
+        layoutView.startDrag(null,
+                builder,
+                null,
+                0);
+
+        builder.getView().setOnDragListener(this);
+
+    }
+
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d(TAG,"Action was onFling");
+        return false;
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        switch(event.getAction()) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+                Log.d(TAG, "onDrag: drag started.");
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.d(TAG, "onDrag: drag entered.");
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                Log.d(TAG, "onDrag: current point: ( " + event.getX() + " , " + event.getY() + " )"  );
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                Log.d(TAG, "onDrag: exited.");
+                return true;
+
+            case DragEvent.ACTION_DROP:
+
+                Log.d(TAG, "onDrag: dropped.");
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                Log.d(TAG, "onDrag: ended.");
+
+
+                return true;
+
+            // An unknown action type was received.
+            default:
+                Log.e(TAG,"Unknown action type received by OnStartDragListener.");
+                break;
+
+        }
+        return false;
     }
 }
