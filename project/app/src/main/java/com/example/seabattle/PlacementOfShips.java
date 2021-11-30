10ca543ae064621f6a0f124caad7cb59fbfd66de
Cat Crusader <cat.crusader31@gmail.com>
2021-11-30 17:02:23 +0200

    create main menu UI, and game activities

--- project/app/src/main/java/com/example/seabattle/PlacementOfShips.java
+++ project/app/src/main/java/com/example/seabattle/PlacementOfShips.java
@@ -2,15 +2,13 @@ package com.example.seabattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PlacementOfShips extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_placement_of_ships);
     }
 }
